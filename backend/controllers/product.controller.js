65daa52ef1ef46e973fea7688d968b4c41f9dc17
Yashuchirag <chiragchandrashekar@gmail.com>
2025-09-11 13:46:15 -0700

    Updated Backend

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,56 @@
+import Product from "../models/product.model.js";
+import mongoose from "mongoose";
+
+export const getProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.log("Error in fetching products: ", error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
+
+export const createProduct = async (req, res) => {
+    console.log(req.body);
+    const product = req.body;
+    
+    if(!product.name || !product.price || !product.image) {
+        return res.status(400).json({ message: "Missing required fields", success: false });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+};
+
+export const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ message: "Invalid ID", success: false });
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
+        console.log("Error in updating product: ", error);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+};
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted successfully" });
+    } catch (error) {
+        console.log("Error in deleting product: ", error);
+        res.status(500).json({ success: false, message: "Product not found" });
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
 
 const app = express();
 
+app.use(express.json()); // Middleware allows to access the request body
+
+app.use("/api/products", productRoutes);
+
 app.listen(3000, () => {
+    connectDB();
     console.log("Server running on port 3000");
 });
+
