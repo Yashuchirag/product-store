e451ea27f5270bdada03547d3fc3d749cfebccc0
Yashuchirag <chiragchandrashekar@gmail.com>
2025-09-12 15:17:20 -0700

    Updated to package.json and server.js to deploy in single port

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,43 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import path from "path";
+import { fileURLToPath } from "url";
+import cors from "cors";
 
 import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json()); // Middleware allows to access the request body
+app.use(cors());
 
 app.use("/api/products", productRoutes);
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get(/^\/.*$/, (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
     });
 }
 
-app.listen(PORT, () => {
-    connectDB();
-    console.log("Server running on port http://localhost:" + PORT);
-});
+// Start Server after DB connection
+const startServer = async () => {
+    try {
+      await connectDB();
+      app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.error("Failed to connect to DB", error);
+      process.exit(1);
+    }
+  };
+  
+  startServer();
 
